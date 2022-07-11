6c317ec4618e50b7694939eeceb8d30f32ff7768
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-07-11 12:43:41 +0100

    Created simple counter

---
+++ Counter/script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const countOutput = document.querySelector("input#numbers");
+const countHistory = document.querySelector("#countHistory");
+let clickCount = 0;
+
+const addedTen = () => {
+    clickCount = clickCount+ 1;
+    countOutput.value = parseInt(countOutput.value) + 10;
+    const countLog = document.createElement("p");
+    countLog.innerText = clickCount + " : Added 10 : " + countOutput.value;
+    countHistory.appendChild(countLog);
+}
+
+const addedFive = () => {
+    clickCount = clickCount+ 1;
+    countOutput.value = parseInt(countOutput.value) + 5;
+    const countLog = document.createElement("p");
+    countLog.innerText = clickCount + " : Added 5 : "+ countOutput.value;
+    countHistory.appendChild(countLog);
+}
+
+const minusFive = () => {
+    clickCount = clickCount+ 1;
+    countOutput.value = parseInt(countOutput.value) - 5;
+    const countLog = document.createElement("p");
+    countLog.innerText = clickCount + " : Subtracted 5 : " + countOutput.value;
+    countHistory.appendChild(countLog);
+}
+
+const minusTen = () => {
+    clickCount = clickCount+ 1;
+    countOutput.value = parseInt(countOutput.value) - 10;
+    const countLog = document.createElement("p");
+    countLog.innerText = clickCount + " : Subtracted 10 : "+ countOutput.value;
+    countHistory.appendChild(countLog);
+}
+
+const reset = () => {
+    clickCount = clickCount+ 1;
+    countOutput.value = 0;
+    const countLog = document.createElement("p");
+    countLog.innerText = clickCount + " : Cleared";
+    countHistory.appendChild(countLog);
+}
